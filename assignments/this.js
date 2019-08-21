d65c0946fe74314c3c4147c32512e3e909cdfab1
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-08-21 12:44:49 +0100

    answer questions about 'this' rules

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. The default value of 'this' is the Window Binding. This means that outside of a function, or within a normal function, the word 'this' points to the window.
+
+* 2. Implicit Binding is when 'this' is contained within a function that is called as a method. In this scenario, 'this' refers to the object that the method is being called on.
+
+* 3. New Binding refers to when a Constructor function is being called. In this case, 'this' refers to the object that the constructor is creating.
+
+* 4. Explicit Binding: in the case of invoking functions with call and apply, or copying functions with bind, you can assign 'this' within the brackets to whatever you want. 
 *
 * write out a code example of each explanation above
 */
